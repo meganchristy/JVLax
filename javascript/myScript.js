97fef97af0a8f6311d1a99f3bdd7b143d4e87e34
meganchristy <meganchristy@stmarksschool.org>
2016-05-04 17:55:02 -0400

    first

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,12 +1,7 @@
 //JQuery
 
 $(document).ready(function(){
-    $('.fadein img:gt(0)').hide();
-    setInterval(function(){
-      $('.fadein :first-child').fadeOut()
-         .next('img').fadeIn()
-         .end().appendTo('.fadein');}, 
-      3000);
+  
 });
 
 //javascript
@@ -77,5 +72,76 @@ window.onload = function() {
 
 		document.getElementById("picView").src = picList[whichPic];
 		document.getElementById("caption").innerHTML = captionData[whichPic];
+	});
+};
+
+//javascript for govs
+
+var govsPicList = new Array(13);
+var govsCaptionData = new Array(13);
+var govsWhichPic = 0; //0 means first pic
+
+var govsFButtonListener;
+var govsBButtonListener;
+
+	//populate array
+	govsPicList[0] = "images/govs/p0.jpg";
+	govsPicList[1] = "images/govs/p1.jpg";
+	govsPicList[2] = "images/govs/p2.jpg";
+	govsPicList[3] = "images/govs/p3.jpg";
+	govsPicList[4] = "images/govs/p4.jpg";
+	govsPicList[5] = "images/govs/p5.jpg";
+	govsPicList[6] = "images/govs/p60.jpg";
+	govsPicList[7] = "images/govs/p7.jpg";
+	govsPicList[8] = "images/govs/p8.jpg";
+	govsPicList[9] = "images/govs/p9.jpg";
+	govsPicList[10] = "images/govs/p10.jpg";
+	govsPicList[11] = "images/govs/p11.jpg";
+	govsPicList[12] = "images/govs/p12.jpg";
+
+	govsCaptionData[0] = "Taylor working the net";
+	govsCaptionData[1] = "Megan looking for a pass";
+	govsCaptionData[2] = "Blaine heading to net";
+	govsCaptionData[3] = "Ainsley getting open";
+	govsCaptionData[4] = "Hannah cradling down the field";
+	govsCaptionData[5] = "Megan getting ready at the draw";
+	govsCaptionData[6] = "Cecily in position";
+	govsCaptionData[7] = "Katherine and Cecily ready for anything";
+	govsCaptionData[8] = "The team heading to the bench for halftime";
+	govsCaptionData[9] = "Ginny and Mary fighting for the ground ball";
+	govsCaptionData[10] = "Ginny reaching for the ball";
+	govsCaptionData[11] = "Mary getting set on defense";
+	govsCaptionData[12] = "Ainsley transitioning the ball down the field";
+
+
+
+window.onload = function() {
+
+	document.getElementById("govsPicView").src = govsPicList[govsWhichPic];
+	document.getElementById("govsCaption").innerHTML = govsCaptionData[govsWhichPic];
+
+
+	govsFButtonListener = document.getElementById("govsForwardButton");
+	govsBButtonListener = document.getElementById("govsBackButton");
+
+	govsFButtonListener.addEventListener('click',function(event){
+		++govsWhichPic;
+		if(govsWhichPic >= govsPicList.length){
+			govsWhichPic = 0;
+		}
+
+		document.getElementById("govsPicView").src = govsPicList[govsWhichPic];
+		document.getElementById("govsCaption").innerHTML = govsCaptionData[govsWhichPic];
+
+	});
+
+	govsBButtonListener.addEventListener('click',function(event){
+		--govsWhichPic;
+		if (govsWhichPic < 0){
+			govsWhichPic = govsPicList.length-1
+		}
+
+		document.getElementById("govsPicView").src = govsPicList[govsWhichPic];
+		document.getElementById("govsCaption").innerHTML = govsCaptionData[govsWhichPic];
 	});
 };
